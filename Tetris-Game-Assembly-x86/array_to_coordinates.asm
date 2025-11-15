cc237688dc84ed2bdb5daa92cc6bd41627043cfb
hmad213 <142343785+hmad213@users.noreply.github.com>
2025-11-15 14:45:36 +0500

    Create game loop and more variables

--- Tetris-Game-Assembly-x86/array_to_coordinates.asm
+++ Tetris-Game-Assembly-x86/array_to_coordinates.asm
@@ -3,5 +3,6 @@ INCLUDE globals.inc
 .data
 .code
 mapArray PROC		; Turns piece array into coordinates of board array
+	
 mapArray ENDP
 END
--- Tetris-Game-Assembly-x86/game.asm
+++ Tetris-Game-Assembly-x86/game.asm
@@ -1,8 +1,26 @@
 INCLUDE globals.inc
 
+PUBLIC startGame
+EXTERN drawBoard@0:PROC
+
 .data
+	currentPiece DWORD ?
+	nextPiece DWORD ?
+	currentPieceCoordinates DWORD 4 dup(?)
 .code
 startGame PROC		; contains the game loop. also initializes everything before the loop
+
+gameloop:
+	mov dl, 0
+	mov dh, 0
+	call Gotoxy
+
+	call drawBoard@0
+
+	mov eax, 100
+	call Delay
+	jmp gameloop
+
 startGame ENDP
 
 updateGame PROC		; updates the game after each loop
